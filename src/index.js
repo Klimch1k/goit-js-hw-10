a04727ef9f30199ea5bf7ae02ad82d780bf0feda
Klimch1k <114351322+Klimch1k@users.noreply.github.com>
2023-03-26 23:10:05 +0300

    half of the task

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+const DEBOUNCE_DELAY = 300;
+const inputSearchCountry = document.querySelector('#search-box');
+const listCountries = document.querySelector('.country-list');
+const countryInfoBox = document.querySelector('.country-info');
+
+inputSearchCountry.addEventListener(
+  'input',
+  debounce(handleInputChange, DEBOUNCE_DELAY)
+);
+
+function handleInputChange(evt) {
+  const inputValue = evt.target.value
+
+  if (inputValue.trim() === "") {
+    return
+  }
+  fetchCountries(inputValue).then(data => {
+  
+    if (data.length > 10) {
+      Notiflix.Notify.info(
+        'Too many matches found. Please enter a more specific name.'
+      );
+    }
+  });
+    
+
+}
